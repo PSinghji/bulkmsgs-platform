4a1abac46b0cd66dc7136d862e6519bb5ce8fe48
PSinghji <pstanwarec@gmail.com>
2026-06-24 14:47:35 +0530

    feat: added official brand SVG favicon and configured static file serving

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from app.database import pricing_collection, leads_collection
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI(title="BulkMsgs AI Platform")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
